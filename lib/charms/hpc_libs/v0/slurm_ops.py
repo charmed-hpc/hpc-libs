b72ce3443584b584e846d24e25c010d6f4e7c268
José Julián Espina <julian.espina@canonical.com>
2024-07-12 13:33:10 -0600

    feat: add method to get the host name

--- lib/charms/hpc_libs/v0/slurm_ops.py
+++ lib/charms/hpc_libs/v0/slurm_ops.py
@@ -68,6 +68,7 @@ __all__ = [
 import json
 import logging
 import re
+import socket
 import subprocess
 from collections.abc import Mapping
 from enum import Enum
@@ -83,7 +84,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 # Charm library dependencies to fetch during `charmcraft pack`.
 PYDEPS = ["pyyaml>=6.0.1"]
@@ -291,3 +292,8 @@ class SlurmManagerBase(ServiceManager):
         self._service = service
         self.config = ConfigurationManager(service.config_name)
         self.munge = MungeManager()
+
+    @property
+    def hostname(self) -> str:
+        """The hostname where this manager is running."""
+        return socket.gethostname().split(".")[0]
