44b96dc96e90141614be4f96011412dcfc961ac8
José Julián Espina <julian.espina@canonical.com>
2024-09-06 16:26:09 -0600

    feat: abstract around base package manager
    This should enable switching between snaps and debs in any charm using the library.
    
    It also exposes the slurmutils config models instead of having wrapper methods around it, which overall moves the burden of config APIs to slurmutils.

--- tests/integration/slurm_ops/test_manager.py
+++ tests/integration/slurm_ops/test_manager.py
@@ -3,69 +3,70 @@
 # See LICENSE file for licensing details.
 
 import base64
+from pathlib import Path
 
 import pytest
 
-import lib.charms.hpc_libs.v0.slurm_ops as slurm
-from lib.charms.hpc_libs.v0.slurm_ops import ServiceType, SlurmManagerBase
+from lib.charms.hpc_libs.v0.slurm_ops import SlurmctldManager
 
 
 @pytest.fixture
-def slurmctld() -> SlurmManagerBase:
-    return SlurmManagerBase(ServiceType.SLURMCTLD)
+def slurmctld() -> SlurmctldManager:
+    return SlurmctldManager(snap=True)
 
 
 @pytest.mark.order(1)
-def test_install(slurmctld: SlurmManagerBase) -> None:
+def test_install(slurmctld: SlurmctldManager) -> None:
     """Install Slurm using the manager."""
-    slurm.install()
-    slurmctld.munge.generate_key()
+    slurmctld.install()
+    slurmctld.munge.key.generate()
 
     with open("/var/snap/slurm/common/etc/munge/munge.key", "rb") as f:
         key: str = base64.b64encode(f.read()).decode()
 
-    assert key == slurmctld.munge.get_key()
+    assert key == slurmctld.munge.key.get()
 
 
 @pytest.mark.order(2)
-def test_rotate_key(slurmctld: SlurmManagerBase) -> None:
+def test_rotate_key(slurmctld: SlurmctldManager) -> None:
     """Test that the munge key can be rotated."""
-    old_key = slurmctld.munge.get_key()
-    slurmctld.munge.generate_key()
-    new_key = slurmctld.munge.get_key()
+    old_key = slurmctld.munge.key.get()
+    slurmctld.munge.key.generate()
+    new_key = slurmctld.munge.key.get()
     assert old_key != new_key
 
 
 @pytest.mark.order(3)
-def test_slurm_config(slurmctld: SlurmManagerBase) -> None:
+def test_slurm_config(slurmctld: SlurmctldManager) -> None:
     """Test that the slurm config can be changed."""
-    slurmctld.config.set({"slurmctld-host": "test-slurm-ops", "cluster-name": "test-cluster"})
-    value = slurmctld.config.get("cluster-name")
-    assert value == "test-cluster"
+    with slurmctld.config() as config:
+        config.slurmctld_host = ["test-slurm-ops"]
+        config.cluster_name = "test-cluster"
 
-    with open("/var/snap/slurm/common/etc/slurm/slurm.conf", "r") as f:
-        output = f.read()
+    print(Path("/var/snap/slurm/common/etc/slurm/slurm.conf").read_text())
 
-    for line in output.splitlines():
+    for line in Path("/var/snap/slurm/common/etc/slurm/slurm.conf").read_text().splitlines():
         entry = line.split("=")
         if len(entry) != 2:
             continue
         key, value = entry
         if key == "ClusterName":
             assert value == "test-cluster"
+        if key == "SlurmctldHost":
+            assert value == "test-slurm-ops"
 
 
 @pytest.mark.order(4)
-def test_enable_service(slurmctld: SlurmManagerBase) -> None:
+def test_enable_service(slurmctld: SlurmctldManager) -> None:
     """Test that the slurmctl daemon can be enabled."""
-    slurmctld.enable()
-    assert slurmctld.active()
+    slurmctld.service.enable()
+    assert slurmctld.service.active()
 
 
 @pytest.mark.order(5)
-def test_version() -> None:
+def test_version(slurmctld: SlurmctldManager) -> None:
     """Test that the Slurm manager can report its version."""
-    version = slurm.version()
+    version = slurmctld.version()
 
     # We are interested in knowing that this does not return a falsy value (`None`, `''`, `[]`, etc.)
     assert version
