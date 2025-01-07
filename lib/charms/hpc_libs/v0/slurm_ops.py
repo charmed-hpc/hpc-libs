beab8c84aec82fb8264fc4767ca04f014c5c0e21
dsloanm <dominic.sloanmurphy@canonical.com>
2025-01-07 17:45:21 +0000

    chore(slurm_ops) increment `LIBPATCH` to 12

--- lib/charms/hpc_libs/v0/slurm_ops.py
+++ lib/charms/hpc_libs/v0/slurm_ops.py
@@ -109,7 +109,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 11
+LIBPATCH = 12
 
 # Charm library dependencies to fetch during `charmcraft pack`.
 PYDEPS = [
