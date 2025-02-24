d3bdec33734e66646ef623dcaf7192de432e42c4
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2025-02-24 16:17:51 -0500

    chore(slurm_ops): bump `slurm_ops` to 0.17
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- lib/charms/hpc_libs/v0/slurm_ops.py
+++ lib/charms/hpc_libs/v0/slurm_ops.py
@@ -110,7 +110,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 16
+LIBPATCH = 17
 
 # Charm library dependencies to fetch during `charmcraft pack`.
 PYDEPS = [
