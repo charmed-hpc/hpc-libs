120e635bd2b3828b35341a9312ed8d629371f364
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2025-07-10 15:19:38 -0400

    feat(interfaces): convert `block_when` and `wait_when` to partial functions
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- src/hpc_libs/interfaces/base.py
+++ src/hpc_libs/interfaces/base.py
@@ -17,8 +17,9 @@
 __all__ = [
     "ConditionEvaluation",
     "Condition",
-    "Secret",
     "Interface",
+    "update_secret",
+    "load_secret",
     "update_app_data",
     "integration_exists",
     "integration_not_exists",
@@ -29,8 +30,8 @@ __all__ = [
 import json
 import logging
 from collections.abc import Callable, Mapping
-from functools import wraps
-from typing import Any, Self
+from functools import partial, wraps
+from typing import Any
 
 import ops
 
@@ -42,59 +43,37 @@ type ConditionEvaluation = tuple[bool, str]
 type Condition = Callable[[ops.CharmBase], ConditionEvaluation]
 
 
-class Secret:
-    """Wrapper for interfacing with Juju secrets."""
+def update_secret(charm: ops.CharmBase, label: str, content: dict[str, str]) -> ops.Secret:
+    """Update a secret.
 
-    def __init__(self, secret: ops.Secret) -> None:
-        self._secret = secret
+    Args:
+        charm: Charm to associate secret with.
+        label: Secret label.
+        content: Payload to set as secret content.
 
-    @property
-    def uri(self) -> str:
-        """Get the URI of this secret."""
-        return self._secret.id or ""
+    Notes:
+        - The secret will be created if it does not exist.
+    """
+    try:
+        secret = charm.model.get_secret(label=label)
+        secret.set_content(content=content)
+    except ops.SecretNotFoundError:
+        secret = charm.app.add_secret(label=label, content=content)
 
-    @classmethod
-    def create_or_update(cls, charm: ops.CharmBase, label: str, content: dict[str, str]) -> Self:
-        """Create or update a secret.
+    return secret
 
-        Args:
-            charm: Charm to associate secret with.
-            label: Secret label.
-            content: Payload to set as secret content.
-        """
-        try:
-            secret = charm.model.get_secret(label=label)
-            secret.set_content(content=content)
-        except ops.SecretNotFoundError:
-            secret = charm.app.add_secret(label=label, content=content)
 
-        return cls(secret)
+def load_secret(charm: ops.CharmBase, label: str) -> ops.Secret | None:
+    """Load a secret.
 
-    @classmethod
-    def load(cls, charm: ops.CharmBase, label: str) -> Self | None:
-        """Load a secret.
-
-        Args:
-            charm: Charm to load secret from.
-            label: Secret label.
-        """
-        try:
-            secret = charm.model.get_secret(label=label)
-            return cls(secret)
-        except ops.SecretNotFoundError:
-            return None
-
-    def grant(self, relation: ops.Relation) -> None:
-        """Grant read access to this secret.
-
-        Args:
-            relation: Integration to grant read access to.
-        """
-        self._secret.grant(relation)
-
-    def remove(self) -> None:
-        """Remove all revisions of this secret."""
-        self._secret.remove_all_revisions()
+    Args:
+        charm: Charm to load secret from.
+        label: Secret label.
+    """
+    try:
+        return charm.model.get_secret(label=label)
+    except ops.SecretNotFoundError:
+        return None
 
 
 class Interface(ops.Object):
@@ -159,14 +138,9 @@ class Interface(ops.Object):
                 else False
             )
 
-        try:
-            integration = [
-                integration
-                for integration in self.integrations
-                if integration.id == integration_id
-            ][0]
+        if integration := self.get_integration(integration_id):
             return self._is_integration_ready(integration)
-        except IndexError:
+        else:
             raise IndexError(f"integration id {integration_id} does not exist")
 
     def joined(self) -> bool:
@@ -248,13 +222,14 @@ def integration_not_exists(name: str) -> Condition:
     return wrapper
 
 
-def block_when(*conditions: Condition) -> Callable[..., Any]:
-    """Evaluate blocking conditions.
+def _status_when(*conditions: Condition, status: type[ops.StatusBase]) -> Callable[..., Any]:
+    """Evaluate conditions.
 
-    If a condition is `True`, set a `BlockedStatus` message.
+    If a condition is `True`, set a new status message.
 
     Args:
         *conditions: Conditions to evaluate.
+        status: Status type to set if a condition is evaluated to be `True`.
     """
 
     def decorator(func: Callable[..., Any]):
@@ -266,8 +241,19 @@ def block_when(*conditions: Condition) -> Callable[..., Any]:
             for condition in conditions:
                 result, msg = condition(charm)
                 if result:
+                    _logger.debug(
+                        (
+                            "condition '%s' evaluated to be `True`. deferring event `%s` and "
+                            + "updating status of unit %s to `%s` with message '%s'"
+                        ),
+                        condition.__name__,
+                        event,
+                        charm.unit.name,
+                        status.__name__,
+                        msg,
+                    )
                     event.defer()
-                    raise StopCharm(ops.BlockedStatus(msg))
+                    raise StopCharm(status(msg))
 
             return func(charm, *args, **kwargs)
 
@@ -276,29 +262,5 @@ def block_when(*conditions: Condition) -> Callable[..., Any]:
     return decorator
 
 
-def wait_when(*conditions: Condition) -> Callable[..., Any]:
-    """Evaluate awaitable conditions.
-
-    If a condition is `True`, set a `WaitingStatus` message.
-
-    Args:
-        *conditions: Conditions to evaluate.
-    """
-
-    def decorator(func: Callable[..., Any]):
-        @wraps(func)
-        def wrapper(charm: ops.CharmBase, *args: ops.EventBase, **kwargs: Any) -> Any:
-            event, *_ = args
-            _logger.debug("handling event `%s` on unit %s", event, charm.unit.name)
-
-            for condition in conditions:
-                result, msg = condition(charm)
-                if result:
-                    event.defer()
-                    raise StopCharm(ops.WaitingStatus(msg))
-
-            return func(charm, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
+block_when = partial(_status_when, status=ops.BlockedStatus)
+wait_when = partial(_status_when, status=ops.WaitingStatus)
