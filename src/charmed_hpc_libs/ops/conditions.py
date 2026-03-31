9e404ec83736ef2897e28b89ddaf17bbdbca1dec
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2026-03-31 14:31:00 -0400

    feat(ops/conditions): move conditions pattern into dedicated module
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

---
+++ src/charmed_hpc_libs/ops/conditions.py
@@ -0,0 +1,208 @@
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The conditions pattern.
+
+Use conditions to control when event handlers and charm helper methods are called.
+"""
+
+__all__ = [
+    "Condition",
+    "ConditionEvaluation",
+    "StopCharm",
+    "refresh",
+    "leader",
+    "integration_exists",
+    "integration_not_exists",
+    "block_unless",
+    "wait_unless",
+]
+
+from collections.abc import Callable
+from functools import partial, wraps
+from typing import Any, NamedTuple
+
+import ops
+
+from .logging import setup_logging
+
+_logger = setup_logging(__name__)
+
+
+class ConditionEvaluation(NamedTuple):  # noqa D101
+    ok: bool
+    message: str = ""
+
+
+type Condition[T: ops.CharmBase] = Callable[[T], ConditionEvaluation]
+
+
+class StopCharm(Exception):  # noqa N818
+    """Exception raised to set high-priority status message."""
+
+    @property
+    def status(self) -> ops.StatusBase:
+        """Get charm status passed as the first argument to this exception."""
+        return self.args[0]
+
+
+def refresh[T: ops.CharmBase](hook: Callable[[T], ops.StatusBase] | None = None) -> Callable:
+    """Refresh a charm's status after running an event handler.
+
+    Args:
+        hook: State check hook to call after event handler completes.
+    """
+
+    def decorator(func: Callable[..., None]) -> Callable:
+        @wraps(func)
+        def wrapper(charm: T, *args: ops.EventBase, **kwargs: Any) -> None:
+            event, *_ = args
+
+            try:
+                func(charm, *args, **kwargs)
+            except StopCharm as e:
+                _logger.debug(
+                    (
+                        "`StopCharm` exception raised while running `%s` event handler `%s.%s` ",
+                        "on unit '%s'. setting status to `%s`",
+                    ),
+                    event.__class__.__name__,
+                    charm.__class__.__name__,
+                    func.__name__,
+                    charm.unit.name,  # type: ignore
+                    e.status,
+                )
+                charm.unit.status = e.status
+                return
+
+            if hook is not None:
+                _logger.debug(
+                    "running status check function `%s` to determine new status for unit '%s'",
+                    hook.__name__,
+                    charm.unit.name,  # type: ignore
+                )
+                status = hook(charm)
+                _logger.debug(
+                    "new status for unit '%s' determined to be `%s`",
+                    charm.unit.name,  # type: ignore
+                    status,
+                )
+                charm.unit.status = status
+
+        return wrapper
+
+    return decorator
+
+
+def leader(func: Callable[..., Any]) -> Callable[..., Any]:
+    """Only run method if the unit is the application leader, otherwise skip."""
+
+    @wraps(func)
+    def wrapper(charm: ops.CharmBase, *args: Any, **kwargs: Any) -> Any:
+        if not charm.unit.is_leader():
+            _logger.debug(
+                (
+                    "unit '%s' is not the leader of the '%s' application, ",
+                    "skipping run of method `%s.%s`",
+                ),
+                charm.unit.name,
+                charm.app.name,
+                charm.__class__.__name__,
+                func.__name__,
+            )
+            return None
+
+        _logger.debug(
+            "unit '%s' is the leader of the '%s' application, running method `%s.%s`",
+            charm.unit.name,
+            charm.app.name,
+            charm.__class__.__name__,
+            func.__name__,
+        )
+        return func(charm, *args, **kwargs)
+
+    return wrapper
+
+
+def integration_exists(name: str) -> Condition:
+    """Check if an integration exists.
+
+    Args:
+        name: Name of integration to check existence of.
+    """
+
+    def wrapper(charm: ops.CharmBase) -> ConditionEvaluation:
+        return ConditionEvaluation(bool(charm.model.relations.get(name)), "")
+
+    return wrapper
+
+
+def integration_not_exists(name: str) -> Condition:
+    """Check if an integration does not exist.
+
+    Args:
+        name: Name of integration to check existence of.
+    """
+
+    def wrapper(charm: ops.CharmBase) -> ConditionEvaluation:
+        not_exists = not bool(charm.model.relations.get(name))
+        return ConditionEvaluation(
+            not_exists, f"Waiting for integrations: [`{name}`]" if not_exists else ""
+        )
+
+    return wrapper
+
+
+def _status_unless(*conditions: Condition, status: type[ops.StatusBase]) -> Callable[..., Any]:
+    """Evaluate conditions.
+
+    If a condition is `False`, set a new status message.
+
+    Args:
+        *conditions: Conditions to evaluate.
+        status: Status type to set if a condition is evaluated to be `False`.
+    """
+
+    def decorator(func: Callable[..., Any]):
+        @wraps(func)
+        def wrapper(charm: ops.CharmBase, *args: ops.EventBase, **kwargs: Any) -> Any:
+            event, *_ = args
+            _logger.debug("handling event `%s` on %s", event, charm.unit.name)
+
+            for condition in conditions:
+                ok, message = condition(charm)
+                if not ok:
+                    _logger.debug(
+                        (
+                            "condition '%s' evaluated to be `False`. deferring event `%s` and "
+                            + "updating status of unit %s to `%s` with message '%s'"
+                        ),
+                        condition.__name__,
+                        event,
+                        charm.unit.name,
+                        status.__name__,
+                        message,
+                    )
+                    event.defer()
+                    raise StopCharm(status(message))
+
+            return func(charm, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+block_unless = partial(_status_unless, status=ops.BlockedStatus)
+wait_unless = partial(_status_unless, status=ops.WaitingStatus)
---
+++ tests/unit/ops/test_conditions.py
@@ -0,0 +1,136 @@
+# Copyright 2025-2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for the conditions pattern."""
+
+import ops
+import pytest
+from ops import testing
+
+from charmed_hpc_libs import (
+    ConditionEvaluation,
+    block_unless,
+    integration_exists,
+    integration_not_exists,
+    leader,
+    refresh,
+    wait_unless,
+)
+
+
+def service_is_active(_: ops.CharmBase) -> ConditionEvaluation:
+    return ConditionEvaluation(False, "Waiting for service to start")
+
+
+def unit_is_leader(charm: ops.CharmBase) -> ConditionEvaluation:
+    is_leader = charm.unit.is_leader()
+    return ConditionEvaluation(is_leader, "Unit is not leader" if not is_leader else "")
+
+
+def post_handler_checks(charm: ops.CharmBase) -> ops.StatusBase:
+    condition = integration_exists("database")(charm)
+    if not condition.ok:
+        return ops.BlockedStatus("Database integration is missing")
+
+    condition = integration_not_exists("database-proxy")(charm)
+    if not condition.ok:
+        return ops.BlockedStatus("Database is connected. Proxy is unnecessary")
+
+    condition = integration_not_exists("metrics-server")(charm)
+    if condition.ok:
+        return ops.BlockedStatus(condition.message)
+
+    return ops.ActiveStatus()
+
+
+class MockCharm(ops.CharmBase):
+    """Mock charm for testing the conditions pattern."""
+
+    def __init__(self, framework: ops.Framework) -> None:
+        super().__init__(framework)
+
+        framework.observe(self.on.install, self._on_install)
+        framework.observe(self.on.config_changed, self._on_config_changed)
+        framework.observe(self.on.update_status, self._on_update_status)
+        framework.observe(self.on["database"].relation_created, self._on_database_relation_created)
+
+    @refresh(hook=None)
+    @block_unless(unit_is_leader)
+    def _on_install(self, _: ops.InstallEvent) -> None: ...
+
+    @leader
+    @block_unless(unit_is_leader)  # Ensure that `leader` is called before `block_unless`.
+    def _on_config_changed(self, _: ops.ConfigChangedEvent) -> None:
+        self.unit.status = ops.MaintenanceStatus("Leader is updating settings")
+
+    @refresh(hook=post_handler_checks)
+    def _on_update_status(self, _: ops.UpdateStatusEvent) -> None: ...
+
+    @refresh(hook=None)
+    @wait_unless(service_is_active)
+    def _on_database_relation_created(self, _: ops.RelationCreatedEvent) -> None: ...
+
+
+@pytest.fixture(scope="function")
+def mock_charm() -> testing.Context[MockCharm]:
+    return testing.Context(
+        MockCharm,
+        meta={
+            "name": "mock-conditions-charm",
+            "requires": {
+                "database": {"interface": "database"},
+                "database-proxy": {"interface": "database-proxy"},
+            },
+        },
+    )
+
+
+@pytest.mark.parametrize("is_leader", (True, False))
+def test_leader_condition(mock_charm, is_leader) -> None:
+    """Test the `leader` condition."""
+    state = mock_charm.run(mock_charm.on.config_changed(), state=testing.State(leader=is_leader))
+
+    if is_leader:
+        assert state.unit_status == ops.MaintenanceStatus("Leader is updating settings")
+    else:
+        assert state.unit_status == ops.UnknownStatus()
+
+
+def test_block_unless_condition(mock_charm) -> None:
+    """Test the `block_unless` condition."""
+    state = mock_charm.run(mock_charm.on.install(), state=testing.State(leader=False))
+
+    assert state.unit_status == ops.BlockedStatus("Unit is not leader")
+
+
+def test_wait_unless_condition(mock_charm) -> None:
+    """Test the `wait_unless` condition."""
+    integration = testing.Relation(endpoint="database")
+
+    state = mock_charm.run(
+        mock_charm.on.relation_created(integration), state=testing.State(relations={integration})
+    )
+
+    assert state.unit_status == ops.WaitingStatus("Waiting for service to start")
+
+
+def test_refresh(mock_charm) -> None:
+    """Test the `refresh` decorator."""
+    integration = testing.Relation(endpoint="database")
+
+    state = mock_charm.run(
+        mock_charm.on.update_status(), state=testing.State(relations={integration})
+    )
+
+    assert state.unit_status == ops.BlockedStatus("Waiting for integrations: [`metrics-server`]")
